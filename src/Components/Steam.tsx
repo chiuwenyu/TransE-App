e8004e907214ec9070d98221adcd08f2bfafc0a4
WENYU CHIU <chiuwenyu@gmail.com>
2024-04-26 21:58:45 +0800

    implement Steam page components under control

--- src/Components/Steam.tsx
+++ src/Components/Steam.tsx
@@ -12,9 +12,15 @@ import {
   Select,
   MenuItem,
   FormControl,
+  Menu,
 } from "@mui/material";
+import { useState } from "react";
 
 export const Steam = () => {
+  const [temp, setTemp] = useState("0");
+  const [pres, setPres] = useState("0");
+  const [steamState, setSteamState] = useState(0);
+
   return (
     <Grid
       container
@@ -57,33 +63,44 @@ export const Steam = () => {
                 <Select
                   labelId="state-label"
                   id="state-select"
-                  value={10}
+                  value={steamState}
                   label="State"
-                  onChange={() => {
-                    console.log("channged");
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setSteamState(e.target.value as number);
                   }}
                   sx={{ width: "45ch" }}
                 >
+                  <MenuItem value={0}>-- Select a State --</MenuItem>
                   <MenuItem value={10}>Saturated Steam -- T</MenuItem>
                   <MenuItem value={20}>Saturated Steam -- P</MenuItem>
                   <MenuItem value={30}>Superheated steam</MenuItem>
                   <MenuItem value={40}>Water</MenuItem>
                 </Select>
               </FormControl>
-              <TextField
-                id="outlined-basic"
-                label="Temperature (°C)"
-                variant="outlined"
-              />
-              <TextField
-                id="outlined-basic"
-                label="Pressure (kPa)"
-                variant="outlined"
-              />
+              {steamState === 20 || steamState === 0 ? undefined : (
+                <TextField
+                  id="outlined-basic"
+                  label="Temperature (°C)"
+                  variant="outlined"
+                  value={temp}
+                  onChange={(e) => setTemp(e.target.value)}
+                />
+              )}
+              {steamState === 10 || steamState === 0 ? undefined : (
+                <TextField
+                  id="outlined-basic"
+                  label="Pressure (kPa)"
+                  variant="outlined"
+                  value={pres}
+                  onChange={(e) => setPres(e.target.value)}
+                />
+              )}
             </Box>
           </CardContent>
           <CardActions sx={{ ml: 1, mt: 4, mb: 1 }}>
             <Button size="medium">Calculate</Button>
+            <Button size="medium">IAPWS-IF97</Button>
           </CardActions>
         </Card>
       </Grid>
