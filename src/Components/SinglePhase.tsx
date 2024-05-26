86f70fd58dd61e7e8ad17354780f07ce4863bce4
WENYU CHIU <chiuwenyu@gmail.com>
2024-05-26 12:37:46 +0800

    SinglePhase tsx primary layout

--- src/Components/SinglePhase.tsx
+++ src/Components/SinglePhase.tsx
@@ -1,18 +1,80 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import { Grid } from "@mui/material";
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 export const SinglePhase = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <Grid
       container
+      // alignItems="center"
+      gap={6}
       sx={{
         bgcolor: "background.default",
-        pt: 10,
-        fontSize: "10px",
         minHeight: "100vh",
       }}
     >
-      <Grid item xs={12} style={{ textAlign: "center" }}>
-        <h1>Single Phase Line Sizing</h1>
+      <Grid item xs={4} sx={{ ml: 4, mt: 1, pt: 10 }}>
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs">
+              <Tab label="Process" {...a11yProps(0)} />
+              <Tab label="Project" {...a11yProps(1)} />
+              <Tab label="Option" {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            Item One
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            Item Three
+          </CustomTabPanel>
+        </Box>
       </Grid>
     </Grid>
   );
