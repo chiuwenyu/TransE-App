e8b89f714af8031fee0bae86352f0cc0f6d603b5
WENYU CHIU <chiuwenyu@gmail.com>
2024-06-05 22:31:25 +0800

    debug some warnings

--- src/Components/SinglePhase.tsx
+++ src/Components/SinglePhase.tsx
@@ -55,6 +55,7 @@ let res: Result = {
   vh: -999.0,
 };
 
+// 將 num 輸出格式化的 scientific format to 1.23E+002
 function fmt_f64(
   num: number,
   width: number,
@@ -63,17 +64,14 @@ function fmt_f64(
 ): string {
   let numStr: string = num.toExponential(precision);
   let eIndex: number = numStr.indexOf("e");
-
   let exp: string = numStr.slice(eIndex + 1);
   numStr = numStr.slice(0, eIndex);
-
   let sign: string = exp.startsWith("-") ? "-" : "+";
   if (sign === "-") {
     exp = exp.slice(1);
   } else {
     exp = exp.slice(1);
   }
-
   numStr += "E" + sign + exp.padStart(exp_pad, "0");
   return numStr.padStart(width);
 }
@@ -89,11 +87,7 @@ function CustomTabPanel(props: TabPanelProps) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </div>
   );
 }
